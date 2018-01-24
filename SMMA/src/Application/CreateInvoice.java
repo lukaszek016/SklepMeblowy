214bdd31d99ea2987ea48f2a70957e1e707b65f4
Łukasz <ukasz@home1.lan>
2018-01-24 15:43:18 +0100

    Zauktualizowano plik

--- SMMA/src/Application/CreateInvoice.java
+++ SMMA/src/Application/CreateInvoice.java
@@ -16,6 +16,7 @@ public class CreateInvoice extends javax.swing.JFrame {
      */
     public CreateInvoice() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -27,7 +28,7 @@ public class CreateInvoice extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
